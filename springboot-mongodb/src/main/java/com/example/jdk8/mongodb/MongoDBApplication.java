5607544ca772d456b0cd3c3f1fdd0cb75e983bce
zhaojh <1763124707@qq.com>
2024-04-19 13:50:11 +0800

    提交文件

---
+++ springboot-mongodb/src/main/java/com/example/jdk8/mongodb/MongoDBApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdk8.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class MongoDBApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
