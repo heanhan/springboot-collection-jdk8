b01f28e6ca787f8d53180d67ca0155bcc4412d10
zhaojh <1763124707@qq.com>
2024-04-29 15:38:28 +0800

    feat(main): 调整项目结构
    1、使用基于jdk8搭建的环境 用户平时基于jdk8的技术点尝试

--- springboot-arthas-demo/src/main/java/com/example/jdk8/arthas/ArthasApplication.java
+++ springboot-arthas-demo/src/main/java/com/example/jdk8/arthas/ArthasApplication.java
@@ -1,4 +1,4 @@
-package com.example.jdk8.mongodb;
+package com.example.jdk8.arthas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class MongoDBApplication
+public class ArthasApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(MongoDBApplication.class,args);
+        SpringApplication.run(ArthasApplication.class,args);
     }
 }
---
+++ springboot-arthas-demo/src/main/java/com/example/jdk8/arthas/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.jdk8.arthas.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+
+}
