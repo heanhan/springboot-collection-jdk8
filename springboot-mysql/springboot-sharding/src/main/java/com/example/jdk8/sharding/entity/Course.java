ab54e6395b9b3c398aa7ecb8780ad34bb2099e59
zhaojianhua <1763124707@qq.com>
2024-12-16 12:36:47 +0800

    换了新电脑后的一次代码提交。

---
+++ springboot-mysql/springboot-sharding/src/main/java/com/example/jdk8/sharding/entity/Course.java
@@ -0,0 +1,13 @@
+package com.example.jdk8.sharding.entity;
+
+import lombok.Data;
+
+@Data
+public class Course {
+
+    private Long cid;
+    private String cname;
+    private String course;
+    private Long userId;
+    private String cstatus;
+}
