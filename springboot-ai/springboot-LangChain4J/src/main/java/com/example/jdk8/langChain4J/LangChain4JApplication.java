3c15a7041c58fcafbd59d9b3ced74aa16754785b
zhaojianhua <1763124707@qq.com>
2025-02-26 17:52:49 +0800

    feat(添加新模块): 创建 springboot-ai 模块并集成 LangChain4J
    - 新建 springboot-ai 模块的 pom.xml 文件
    - 在根 pom.xml 中添加 springboot-ai模块引用
    - 创建 LangChain4JApplication 启动类- 添加 application.yml 配置文件
    - 新建 springboot-LangChain4J 模块的 pom.xml 文件，集成 LangChain4J 依赖

---
+++ springboot-ai/springboot-LangChain4J/src/main/java/com/example/jdk8/langChain4J/LangChain4JApplication.java
@@ -0,0 +1,18 @@
+package com.example.jdk8.langChain4J;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class LangChain4JApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(LangChain4JApplication.class,args);
+    }
+}
