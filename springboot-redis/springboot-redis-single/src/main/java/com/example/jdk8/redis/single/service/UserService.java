c0d5e1c2fd7da8a88618d6f8c2d7cdd9c81f4c85
zhaojianhua <1763124707@qq.com>
2025-02-26 13:54:28 +0800

    feat(springboot-redis): 新增用户接口及其实现类- 新增 UserService 接口，定义了新增用户的方法 - 实现 UserServiceImpl 类，提供了新增用户的方法实现 - 为用户相关操作提供了基础结构

---
+++ springboot-redis/springboot-redis-single/src/main/java/com/example/jdk8/redis/single/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.jdk8.redis.single.service;
+
+import com.example.jdk8.redis.single.entity.User;
+
+/**
+ * @Description : 用户接口
+ * @Author : zhaojh
+ * @Date : 2025/2/17 14:32
+ */
+
+public interface UserService {
+
+    /**
+     * 新增用户
+     * @return User
+     */
+    User addUserInfo();
+}
---
+++ springboot-redis/springboot-redis-single/src/main/java/com/example/jdk8/redis/single/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.jdk8.redis.single.service.impl;
+
+import com.example.jdk8.redis.single.entity.User;
+import com.example.jdk8.redis.single.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description : TODO
+ * @Author : zhaojh
+ * @Date : 2025/2/17 14:32
+ */
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    /**
+     * 新增用户
+     *
+     * @return User
+     */
+    @Override
+    public User addUserInfo() {
+        return null;
+    }
+}
